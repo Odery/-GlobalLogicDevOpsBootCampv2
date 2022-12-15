3bc6040a28489815c92af1a8eea49564da520f76
Odery <kammurimini@gmail.com>
2022-12-15 13:13:50 +0200

    Fixed a few errors

--- HW 2/nginx.sh
+++ HW 2/nginx.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # update package source
-apt -y update
+apt-get -y update
 
 # install NGINX
-apt -y install nginx
+apt-get -y install nginx
 
 # change default site a little
-cat > /var/www/nginx-default/index.nginx-debian.html << EOL 
+cat > /var/www/html/index.nginx-debian.html << EOL 
 <!DOCTYPE html><html><head><title>Welcome to nginx!</title><style>
 body {width: 35em;margin: 0 auto;font-family: Tahoma, Verdana, Arial, sans-serif;}
 </style></head><body><h1>Stepan Romanchuk HW2</h1><p>GL Home Work No.2.</p>
